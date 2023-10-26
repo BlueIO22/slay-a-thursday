54b6646c062d461e31c5f020d04c39d0f3629857
blueio22 <marsor354@gmail.com>
2023-10-26 14:00:35 +0200

    formatting, and fixing the state to the layout level

--- my-app/src/app/battle/page.tsx
+++ my-app/src/app/battle/page.tsx
@@ -1,48 +1,40 @@
+import { state } from "../layout";
 import styles from "./page.module.css";
-import {state} from "../page";
 
 console.log(state);
 
-function classStr(...classes:string[])
-{
-    return classes.join(' ');
+function classStr(...classes: string[]) {
+  return classes.join(" ");
 }
 
 const Battle = () => {
+  // let enimies = state.battles[state.currentPosition].enimies;
+  let enimies = ["a", "b", "c"];
+  let hand = ["a", "b", "c"];
 
-    // let enimies = state.battles[state.currentPosition].enimies;
-    let enimies = ['a','b','c'];
-    let hand = ['a','b','c'];
+  const enimyDivs = enimies.map((enimy) => {
+    return <div className={styles.character}> </div>;
+  });
 
-    const enimyDivs = enimies.map(
-            (enimy) =>  {
-            return (<div className={styles.character}> </div>);
-            });
+  const cardDivs = hand.map((card) => {
+    return <div className={styles.card}> </div>;
+  });
 
-    const cardDivs = hand.map(
-            (card) =>  {
-            return (<div className={styles.card}> </div>);
-            });
+  return (
+    <div className={styles.main}>
+      <div className={styles.characters}>
+        <div className={classStr(styles.left, styles.character)}> </div>
+        <div className={styles.space}> </div>
+        <div className={styles.right}>{enimyDivs}</div>
+      </div>
 
-    return (<div className={styles.main}>
-
-            <div className={styles.characters}>
-                <div className={classStr(styles.left, styles.character)}> </div>
-                <div className={styles.space}> </div>
-                <div className={styles.right}>
-                {enimyDivs}
-                </div>
-            </div>
-
-            <div className={styles.cards}>
-                <div className={styles.stack}> </div>
-                <div className={styles.hand}> 
-                {cardDivs}
-                </div>
-                <div className={styles.discard}> </div>
-            </div>
-
-        </div>);
+      <div className={styles.cards}>
+        <div className={styles.stack}> </div>
+        <div className={styles.hand}>{cardDivs}</div>
+        <div className={styles.discard}> </div>
+      </div>
+    </div>
+  );
 };
 
 export default Battle;
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,28 +1,6 @@
-import flaskOfPerfection from "./cards/flaskOfPerfection";
-import fred from "./characters/fred";
-import myFirstSlime from "./characters/myFirstSlime";
 import Home from "./home/page";
-import { Battle } from "./lib/battle";
-import State from "./lib/state";
-import flaskOfPerfectionReward from "./rewards/flaskOfPerfectionReward";
-
-export let state: State = {
-  character: fred,
-  battles: [],
-  currentPosition: 0,
-};
 
 export default function () {
-  const exampleBattle: Battle = {
-    name: "example battle",
-    rewards: [flaskOfPerfectionReward],
-    enemies: [myFirstSlime],
-    position: 0,
-    currentTurn: 0,
-  };
-
-  state.battles = [...state.battles, exampleBattle];
-
   return (
     <>
       <Home />
