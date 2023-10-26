891e1455225d39db82b5f9da0293b8dd720e31d6
Duncan McDougall <dncn.mcdougall@gmail.com>
2023-10-26 11:27:28 +0100

    Changesa to the about

--- my-app/src/app/about/page.tsx
+++ my-app/src/app/about/page.tsx
@@ -1,5 +1,8 @@
 const About = () => {
-  return <>This is a super-fun game</>;
+    return <>
+        <p>This is a super-fun game</p>
+        <p>Written by <a href="https://github.com/BlueIO22">Marius</a> and <a href="https://github.com/dncnmcdougall">Duncan</a></p>
+        </>;
 };
 
 export default About;
