7d194d3f5c080126551418b48f3ffa6121b963a8
Duncan McDougall <dncn.mcdougall@gmail.com>
2023-10-26 13:51:58 +0100

    Fixes images.

--- my-app/src/app/components/card.tsx
+++ my-app/src/app/components/card.tsx
@@ -5,8 +5,8 @@ import styles from "./comp.module.css";
 const CardComp = (props: {card:Card}) => {
     const card = props.card;
     return <div className={styles.card}> 
-    <h4> card.name</h4>
-    <image className={styles.image} src={"/images/"+card.imageUrl+".png"}> </image>
+    <p className={styles.cardHeader}> {card.name}</p>
+    <img className={styles.image} src={"/images/"+card.imageUrl+".png"}/>
     <div className={styles.description}>
     {card.description}
     </div>
