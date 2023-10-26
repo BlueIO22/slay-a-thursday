842d6b513ff85ac0af678d1318aed902565e8025
blueio22 <marsor354@gmail.com>
2023-10-26 17:24:12 +0200

    added garbage man

--- my-app/src/app/battles/slimeyGarden.ts
+++ my-app/src/app/battles/slimeyGarden.ts
@@ -1,3 +1,4 @@
+import garbageMan from "../characters/garbageMan";
 import myFirstSlime from "../characters/myFirstSlime";
 import { Battle } from "../lib/battle";
 import flaskOfPerfectionReward from "../rewards/flaskOfPerfectionReward";
@@ -5,7 +6,7 @@ import flaskOfPerfectionReward from "../rewards/flaskOfPerfectionReward";
 export default {
   name: "example battle",
   rewards: [flaskOfPerfectionReward],
-  enemies: [myFirstSlime],
+  enemies: [myFirstSlime, myFirstSlime, garbageMan],
   position: 0,
   currentTurn: 0,
   hand: [],
